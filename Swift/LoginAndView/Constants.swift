0426408d1c9b64ff477f3caab62227690471e794
kwangyingjin <kwangyingjin@outlook.com>
2017-04-02 22:36:10 +0800

    second

---
+++ Swift/LoginAndView/Constants.swift
@@ -0,0 +1,26 @@
+//
+// Copyright 2014-2017 Amazon.com,
+// Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Amazon Software License (the "License").
+// You may not use this file except in compliance with the
+// License. A copy of the License is located at
+//
+//     http://aws.amazon.com/asl/
+//
+// or in the "license" file accompanying this file. This file is
+// distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, express or implied. See the License
+// for the specific language governing permissions and
+// limitations under the License.
+//
+
+import Foundation
+import AWSCognitoIdentityProvider
+
+let CognitoIdentityUserPoolRegion: AWSRegionType = .USEast1
+let CognitoIdentityUserPoolId = "us-east-1_K0qMhReJI"
+let CognitoIdentityUserPoolAppClientId = "4pr2m5e5c2jf9s8tu7kofkdeve"
+let CognitoIdentityUserPoolAppClientSecret = "1ccvh5iph94hde9lds5lrcar1te99hhovmue7i71pvk4frd6m2ej"
+
+let AWSCognitoUserPoolsSignInProviderKey = "UserPool"
